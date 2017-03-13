c326984b35bc45108639db5b014a13053556ac25
oszust002 <kamil.osuch@gmail.com>
2017-03-13 18:43:01 +0100

    Bumped material-ui version. Turned off temporarily hide/expand logic,

--- app/components/ContestsTable/index.js
+++ app/components/ContestsTable/index.js
@@ -6,9 +6,7 @@
 // TODO: move hide/expand logic to container
 import React from 'react';
 // import styled from 'styled-components';
-import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
-import KeyboardArrowDown from 'material-ui/svg-icons/hardware/keyboard-arrow-down';
-import KeyboardArrowUp from 'material-ui/svg-icons/hardware/keyboard-arrow-up';
+import { Table, TableBody, TableHead, TableRow, TableCell } from 'material-ui/Table';
 import { FormattedMessage } from 'react-intl';
 import messages from './messages';
 
@@ -39,42 +37,42 @@ const mockedData = [{
   ],
 }];
 
-function checkName(row, name) {
-  return row.name === name;
-}
-
-function findByPath(data, splittedPath) {
-  const foundRow = data.find((row) => checkName(row, splittedPath[0]));
-  return splittedPath.length === 1 ? foundRow : findByPath(foundRow.children, splittedPath.slice(1));
-}
+// function checkName(row, name) {
+//   return row.name === name;
+// }
+//
+// function findByPath(data, splittedPath) {
+//   const foundRow = data.find((row) => checkName(row, splittedPath[0]));
+//   return splittedPath.length === 1 ? foundRow : findByPath(foundRow.children, splittedPath.slice(1));
+// }
 
 function buildRows(indent, path, data) {
   return [].concat(...data.map((row) => createTableRow(indent, path, row)));
 }
 
-function expandRow(path) {
-  findByPath(mockedData, path.split('->')).expanded = true;
-}
-
-function hideRow(path) {
-  findByPath(mockedData, path.split('->')).expanded = false;
-}
+// function expandRow(path) {
+//   findByPath(mockedData, path.split('->')).expanded = true;
+// }
+//
+// function hideRow(path) {
+//   findByPath(mockedData, path.split('->')).expanded = false;
+// }
 
 function createTableRow(indent, path, row) {
   const rowPath = path === '' ? row.name : path.concat(`->${row.name}`);
-  const expandedIcon = row.children.length > 0 && row.expanded ?
-    <KeyboardArrowDown onClick={() => hideRow(rowPath)} /> : null;
-  const hiddenIcon = row.children.length > 0 && !row.expanded ?
-    <KeyboardArrowUp onClick={() => expandRow(rowPath)} /> : null;
+  const expandedIcon = null;
+    // row.children.length > 0 && row.expanded ? <KeyboardArrowDown onClick={() => hideRow(rowPath)} /> : null;
+  const hiddenIcon = null;
+    // row.children.length > 0 && !row.expanded ? <KeyboardArrowUp onClick={() => expandRow(rowPath)} /> : null;
   const childrenRows = row.expanded ? buildRows(indent + 1, rowPath, row.children) : [];
   const render = (
     <TableRow key={rowPath}>
-      <TableRowColumn style={{ textIndent: 20 * indent }}>
+      <TableCell style={{ textIndent: 20 * indent }}>
         {expandedIcon}{hiddenIcon} {row.name}
-      </TableRowColumn>
-      <TableRowColumn>
+      </TableCell>
+      <TableCell>
         {row.description}
-      </TableRowColumn>
+      </TableCell>
     </TableRow>
   );
   childrenRows.unshift(render);
@@ -84,14 +82,14 @@ function ContestsTable() {
   const rows = buildRows(0, '', mockedData);
   return (
     <div>
-      <Table selectable={false}>
-        <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+      <Table>
+        <TableHead>
           <TableRow>
-            <TableHeaderColumn><FormattedMessage {...messages.name} /></TableHeaderColumn>
-            <TableHeaderColumn><FormattedMessage {...messages.description} /></TableHeaderColumn>
+            <TableCell><FormattedMessage {...messages.name} /></TableCell>
+            <TableCell><FormattedMessage {...messages.description} /></TableCell>
           </TableRow>
-        </TableHeader>
-        <TableBody displayRowCheckbox={false}>
+        </TableHead>
+        <TableBody>
           {rows}
         </TableBody>
       </Table>

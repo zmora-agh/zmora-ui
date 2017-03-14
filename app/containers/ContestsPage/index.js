a9cc567c67e3cb88450fc4f417e91f10626b8b44
Mateusz Nowotyński <maxmati@memleak.pl>
2017-03-14 22:52:49 +0100

    Refactored Contests table

--- app/containers/ContestsPage/index.js
+++ app/containers/ContestsPage/index.js
@@ -4,40 +4,16 @@
  *
  */
 
-import React, { PropTypes } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import { CONTEST_TYPE } from './constants';
 import makeSelectContestsPage from './selectors';
 import ContestsTable from '../../components/ContestsTable';
-import { hideContestRow, expandContestRow } from './actions';
-export class ContestsPage extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
-  constructor() {
-    super();
-    this.expandRow = this.expandRow.bind(this);
-    this.hideRow = this.hideRow.bind(this);
-  }
-  hideRow(id) {
-    this.props.dispatch(hideContestRow(id));
-  }
 
-  expandRow(id) {
-    this.props.dispatch(expandContestRow(id));
-  }
-
-  render() {
-    return (
-      <ContestsTable
-        contests={this.props.contests}
-        expandRow={this.expandRow}
-        hideRow={this.hideRow}
-      />
-    );
-  }
-}
+const ContestsPage = (props) => (<ContestsTable contests={props.contests} />);
 
 ContestsPage.propTypes = {
-  dispatch: PropTypes.func.isRequired,
-  contests: PropTypes.arrayOf(CONTEST_TYPE),
+  contests: React.PropTypes.arrayOf(CONTEST_TYPE),
 };
 
 const mapStateToProps = makeSelectContestsPage;

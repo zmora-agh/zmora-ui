3879a3d5e17e4c26c1fdab6bc3360c7e7cdd8d3f
Marcin Flis <marcin.flis.95@gmail.com>
2017-03-27 23:38:40 +0200

    adjusted problemContentPropTypes

--- app/components/ProblemView/constants.js
+++ app/components/ProblemView/constants.js
@@ -3,6 +3,4 @@ import { PropTypes } from 'react';
 export const problemContentPropTypes = {
   title: PropTypes.node.isRequired,
   description: PropTypes.node.isRequired,
-  input: PropTypes.node.isRequired,
-  output: PropTypes.node.isRequired,
 };

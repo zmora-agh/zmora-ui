5ab0ba0e6012a6db50d20c4337e2f57c3b1f70df
Marcin Flis <marcin.flis.95@gmail.com>
2017-03-28 22:47:26 +0200

    Feature/markdown material (#50)
    * adjusted problemContentPropTypes
    
    * material-ui markdown works in Problem view
    
    * exported Markdown to separate component

--- app/components/ProblemView/constants.js
+++ app/components/ProblemView/constants.js
@@ -1,8 +1,5 @@
 import { PropTypes } from 'react';
 
 export const problemContentPropTypes = {
-  title: PropTypes.node.isRequired,
-  description: PropTypes.node.isRequired,
-  input: PropTypes.node.isRequired,
-  output: PropTypes.node.isRequired,
+  description: PropTypes.string.isRequired,
 };

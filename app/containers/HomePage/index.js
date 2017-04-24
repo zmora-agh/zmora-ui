c5f1abaccdafe3f3c83983512aa1deb48ecead67
Marcin Skowron <mskowron17@gmail.com>
2017-04-24 23:06:57 +0200

    [HomePage 2.0] Paper deleted, paper background functionality changed to cards shadows

--- app/containers/HomePage/index.js
+++ app/containers/HomePage/index.js
@@ -12,7 +12,6 @@
 import React from 'react';
 import { createStyleSheet } from 'jss-theme-reactor';
 import customPropTypes from 'material-ui/utils/customPropTypes';
-import Paper from 'material-ui/Paper';
 import Layout from 'material-ui/Layout';
 import Text from 'material-ui/Text';
 import StatusPanel from '../../components/StatusPanel';
@@ -27,74 +26,72 @@ const styleSheet = createStyleSheet('HomePageLayout', () => ({
   },
   lightTextStyle: {
     color: '#FFFFFF',
-    lineHeight: '30px',
-    fontSize: 21,
+    lineHeight: '32px',
+    fontSize: 28,
   },
   darkTextStyle: {
     color: '#000000',
-    lineHeight: '30px',
-    fontSize: 21,
+    lineHeight: '32px',
+    fontSize: 28,
   },
   logoStyle: {
-    height: 300,
-    width: 205,
+    height: 340,
+    width: 230,
     margin: '0 auto',
   },
 }));
 
 const logo = require('../../logo.jpg');
-const cardHeight = 270;
+const cardHeight = 340;
 
 function HomePage(props, context) {
   const classes = context.styleManager.render(styleSheet);
 
   return (
-    <Paper className={classes.paper}>
-      <Layout container className={classes.root}>
-        <Layout container item xs={12}>
-          <Layout item xs={7}>
-            <Info color="#03A9F4" height={cardHeight}>
-              <Text className={classes.lightTextStyle}>
-                <strong>Zmora</strong> to zautomatyzowana platforma edukacyjna, kierowana do studentów
-                kierunków informatycznych <strong>AGH</strong>. W przystępny sposób sprawdza poprawność i
-                wydajność rozwiązań na specjalnie przystosowanych do tego zadaniach. System ocenia sprawiedliwie
-                wkład pracy oraz postępy każdego z uczestników.</Text>
-            </Info>
-          </Layout>
-          <Layout item xs={5}>
-            <StatusPanel height={cardHeight} />
-          </Layout>
+    <Layout container className={classes.root}>
+      <Layout container item xs={12}>
+        <Layout item xs={7}>
+          <Info color="#03A9F4" height={cardHeight}>
+            <Text className={classes.lightTextStyle}>
+              <strong>Zmora</strong> to zautomatyzowana platforma edukacyjna, kierowana do studentów
+              kierunków informatycznych <strong>AGH</strong>. W przystępny sposób sprawdza poprawność i
+              wydajność rozwiązań na specjalnie przystosowanych do tego zadaniach. System ocenia sprawiedliwie
+              wkład pracy oraz postępy każdego z uczestników.</Text>
+          </Info>
         </Layout>
-        <Layout container item xs={12}>
-          <Layout item xs={4} style={{ textAlign: 'center' }}>
-            <Info color="#4CB050" height={cardHeight + 50}>
-              <img src={logo} className={classes.logoStyle} alt="logo" />
-            </Info>
-          </Layout>
-          <Layout item xs={8}>
-            <Info color="#FEC106" height={cardHeight + 50}>
-              <Text className={classes.darkTextStyle}>
-                Nazwa została wybrana nieprzypadkowo - w mitologii słowiańskiej zmora to istota pół demoniczna,
-                która nocą męczy śpiących i wysysa z nich krew. Taki scenariusz również jest możliwy - wystarczy
-                nie rozwiązywać zadań w terminie lub próbować zaliczać je podstępem.<br /><br />
-                Tak czy inaczej - <strong>powodzenia!</strong>
-              </Text>
-            </Info>
-          </Layout>
-        </Layout>
-        <Layout container item xs={12}>
-          <Layout item xs={12}>
-            <StatusInfoList
-              statuses={[{ title: 'Zaliczone', info: 'Uniknięto zemsty Zmory, zadanie zaliczone' },
-              { title: 'Po terminie', info: 'Do roboty' },
-              { title: 'Niezaliczone', info: 'Zemsta zmory jest bliska, życie Ci nie miłe, wędrowcze?' },
-              { title: 'Tekst', info: 'Co tu własciwie ma być w tej tabelce? :D' },
-              ]}
-            />
-          </Layout>
+        <Layout item xs={5}>
+          <StatusPanel color="#FE5722" height={cardHeight} />
         </Layout>
       </Layout>
-    </Paper>
+      <Layout container item xs={12}>
+        <Layout item xs={4} style={{ textAlign: 'center' }}>
+          <Info color="#4CB050" height={cardHeight + 50}>
+            <img src={logo} className={classes.logoStyle} alt="logo" />
+          </Info>
+        </Layout>
+        <Layout item xs={8}>
+          <Info color="#FEC106" height={cardHeight + 50}>
+            <Text className={classes.darkTextStyle}>
+              Nazwa została wybrana nieprzypadkowo - w mitologii słowiańskiej zmora to istota pół demoniczna,
+              która nocą męczy śpiących i wysysa z nich krew. Taki scenariusz również jest możliwy - wystarczy
+              nie rozwiązywać zadań w terminie lub próbować zaliczać je podstępem.<br /><br />
+              Tak czy inaczej - <strong>powodzenia!</strong>
+            </Text>
+          </Info>
+        </Layout>
+      </Layout>
+      <Layout container item xs={12}>
+        <Layout item xs={12}>
+          <StatusInfoList
+            statuses={[{ title: 'Zaliczone', info: 'Uniknięto zemsty Zmory, zadanie zaliczone' },
+            { title: 'Po terminie', info: 'Do roboty' },
+            { title: 'Niezaliczone', info: 'Zemsta zmory jest bliska, życie Ci nie miłe, wędrowcze?' },
+            { title: 'Tekst', info: 'Co tu własciwie ma być w tej tabelce? :D' },
+            ]}
+          />
+        </Layout>
+      </Layout>
+    </Layout>
   );
 }
 

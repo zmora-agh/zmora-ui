317bb4009a0b08b8af2147d4569280cb61906ebf
Mateusz Nowotyński <maxmati@memleak.pl>
2017-05-19 12:59:41 +0200

    Use graphql for time synchronization

--- app/containers/App/sagas.js
+++ app/containers/App/sagas.js
@@ -1,43 +1,14 @@
-import { call, put, take, takeLatest } from 'redux-saga/effects';
-import moment from 'moment';
+import { put, takeLatest } from 'redux-saga/effects';
 import { push } from 'react-router-redux';
 import jwtDecode from 'jwt-decode';
 
 
-import { getCurrentTimeURL } from '../../urls';
 import { homePage } from '../../local-urls';
-import { fetchWithCredentials } from '../../utils/sagas';
 import { deleteJwtToken, getJwtToken, haveJwtToken } from '../../utils/auth';
 import { submitSaga } from '../Submit/sagas';
-import { LOGIN_SUCCESS } from '../Login/constants';
 import { loginSuccess } from '../Login/actions';
 
-import { getCurrentTimeSuccess } from './actions';
 import { LOGOUT } from './constants';
-
-function fetchCurrentTime() {
-  return fetchWithCredentials(getCurrentTimeURL(), {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-    },
-  }).then((response) => response.json())
-    .then((response) => moment(response.time));
-}
-
-function* sleep(time) {
-  yield new Promise((resolve) => setTimeout(resolve, time));
-}
-
-function* synchronizeTime() {
-// eslint-disable-next-line no-constant-condition
-  while (true) {
-    if (!haveJwtToken()) yield take(LOGIN_SUCCESS);
-    const time = yield call(fetchCurrentTime);
-    yield put(getCurrentTimeSuccess(time));
-    yield* sleep(5 * 60 * 1000);
-  }
-}
 
 function* logout() {
   deleteJwtToken();
@@ -55,7 +26,6 @@ function* decodeJWT() {
 
 // All sagas to be loaded
 export default [
-  synchronizeTime,
   logoutSaga,
   decodeJWT,
   submitSaga,

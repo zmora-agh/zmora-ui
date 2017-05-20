0e9d56032db2092d9d19c80a9c58eadcdbcb8048
Mateusz "maxmati" Nowotyński <maxmati4@gmail.com>
2017-05-20 13:56:42 +0200

    Change graphql api url

--- app/constants.js
+++ app/constants.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://api.zmora-agh.memleak.pl/';
 
-export const GRAPHQL_API_URL = 'http://graphql-api.zmora-agh.memleak.pl/graphql';
+export const GRAPHQL_API_URL = 'https://graphql-api.zmora-agh.memleak.pl/graphql';

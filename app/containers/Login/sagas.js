7914c9b54eb96b5c7dae70ed41b4f6fa0c7de105
Mateusz Nowotyński <maxmati@memleak.pl>
2017-05-20 21:42:56 +0200

    User graphql for login and register mutations

--- app/containers/Login/sagas.js
+++ app/containers/Login/sagas.js
@@ -1,29 +1,32 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
 import jwtDecode from 'jwt-decode';
 import { push } from 'react-router-redux';
-import { loginURL } from '../../urls';
-import { bootstrap, checkedFetch } from '../../utils/sagas';
+import { gql } from 'react-apollo';
+
+
+import { bootstrap } from '../../utils/sagas';
 import { setJwtToken } from '../../utils/auth';
 import { loginError, loginSuccess } from './actions';
 import { LOGIN } from './constants';
 import { homePage } from '../../local-urls';
+import { client } from '../../graphql';
+
+const LoginMutation = gql`
+  mutation LoginMutation($nick: String!, $password: String!){
+   getJwtToken(nick: $nick, password: $password)
+  }
+`;
 
 function sendLogin(credentials) {
-  return checkedFetch(loginURL(), {
-    method: 'POST',
-    body: JSON.stringify(credentials),
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  }).then((response) => response.json());
+  return client.mutate({ mutation: LoginMutation, variables: credentials });
 }
 
 function* login({ credentials, from = homePage() }) {
   try {
     const response = yield call(sendLogin, credentials);
-    const userInfo = jwtDecode(response.token).dat;
-    setJwtToken(response.token);
+    const token = response.data.getJwtToken;
+    const userInfo = jwtDecode(token);
+    setJwtToken(token);
     yield put(loginSuccess(userInfo));
     yield put(push(from));
   } catch (e) {
--- app/containers/Register/sagas.js
+++ app/containers/Register/sagas.js
@@ -1,26 +1,33 @@
 import { takeLatest, call, put } from 'redux-saga/effects';
-import { registerURL } from '../../urls';
-import { bootstrap, checkedFetch } from '../../utils/sagas';
+import { gql } from 'react-apollo';
+import { push } from 'react-router-redux';
+import jwtDecode from 'jwt-decode';
+
+import { bootstrap } from '../../utils/sagas';
 import { registerError, registerSuccess } from './actions';
 import { REGISTER } from './constants';
-import { login } from '../Login/actions';
+import { loginSuccess } from '../Login/actions';
+import { client } from '../../graphql';
+import { homePage } from '../../local-urls';
+
+const RegisterMutation = gql`
+  mutation RegisterMutation($nick: String!, $password: String!, $name: String!, $email: String!){
+    register(nick: $nick, password: $password, name: $name, email: $email)
+  }
+`;
 
 function sendRegister(user) {
-  return checkedFetch(registerURL(), {
-    method: 'POST',
-    body: JSON.stringify(user),
-    headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-    },
-  }).then((response) => response.json());
+  return client.mutate({ mutation: RegisterMutation, variables: user });
 }
 
 function* register({ user }) {
   try {
-    yield call(sendRegister, user);
+    const response = yield call(sendRegister, user);
+    const token = response.data.register;
+    const userInfo = jwtDecode(token);
     yield put(registerSuccess());
-    yield put(login(user.nick, user.password));
+    yield put(loginSuccess(userInfo));
+    yield put(push(homePage()));
   } catch (e) {
     yield put(registerError());
   }

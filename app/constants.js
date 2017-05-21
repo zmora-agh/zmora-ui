d6fee52222f0d0391166d770df51f60215b79971
Mateusz Nowotyński <maxmati@memleak.pl>
2017-05-22 00:19:01 +0200

    Endpoint unification

--- app/constants.js
+++ app/constants.js
@@ -1,3 +1,2 @@
 export const API_URL = 'https://api.zmora-agh.memleak.pl/';
-
-export const GRAPHQL_API_URL = 'https://graphql-api.zmora-agh.memleak.pl/graphql';
+export const GRAPHQL_API_URL = `${API_URL}graphql`;

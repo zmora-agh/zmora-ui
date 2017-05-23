edb8084ee185de67bcd14de4146c2b93c656ae22
Mateusz Nowotyński <maxmati@memleak.pl>
2017-05-23 22:06:46 +0200

    Show result tab if user is contest owner

--- app/containers/ProblemPage/constants.js
+++ app/containers/ProblemPage/constants.js
@@ -8,3 +8,4 @@ export const CONTENT_HASH_PREFIX = 'content';
 export const EXAMPLES_HASH_PREFIX = 'examples';
 export const SUBMITS_HASH_PREFIX = 'submits';
 export const QUESTIONS_HASH_PREFIX = 'questions';
+export const RESULTS_HASH_PREFIX = 'results';
--- app/containers/ProblemPage/index.js
+++ app/containers/ProblemPage/index.js
@@ -8,22 +8,33 @@ import React, { PropTypes } from 'react';
 import { createStyleSheet } from 'jss-theme-reactor';
 import { FormattedMessage } from 'react-intl';
 import { connect } from 'react-redux';
+import { gql, graphql } from 'react-apollo';
 import SwipeableViews from 'react-swipeable-views';
 
 import Paper from 'material-ui/Paper';
 import Tabs, { Tab } from 'material-ui/Tabs';
 import customPropTypes from 'material-ui/utils/customPropTypes';
+import { createStructuredSelector } from 'reselect';
 
 import { submitSetContext } from '../Submit/actions';
 
+import ProblemResultsPage from '../ProblemResultsPage';
 import ProblemContentPage from '../ProblemContentPage';
 import ProblemExamplesPage from '../ProblemExamplesPage';
 import ProblemSubmitsPage from '../ProblemSubmitsPage';
 import QuestionsPage from '../QuestionsPage';
 
 import messages from './messages';
-import { SUBMITS_HASH_PREFIX, CONTENT_HASH_PREFIX, EXAMPLES_HASH_PREFIX, QUESTIONS_HASH_PREFIX } from './constants';
+import {
+  SUBMITS_HASH_PREFIX,
+  CONTENT_HASH_PREFIX,
+  EXAMPLES_HASH_PREFIX,
+  QUESTIONS_HASH_PREFIX,
+  RESULTS_HASH_PREFIX,
+} from './constants';
+
 import SubmitDetails from '../SubmitDetails/index';
+import { makeSelectUser } from '../App/selectors';
 
 const styleSheet = createStyleSheet('ProblemPage', (theme) => ({
   appBar: {
@@ -41,7 +52,19 @@ const parseHash = (hash) => ({
   value: hash.split('=')[1],
 });
 
-@connect(null, (dispatch) => ({ dispatch }))
+const ContestOwners = gql`
+  query ProblemQuestionsForLayout($contestId: Int!) {
+    contest(id: $contestId) {
+      id
+      owners {
+        id
+      }
+    }
+  }
+`;
+
+@graphql(ContestOwners, { options: (props) => ({ variables: { contestId: getIds(props).contestId } }) })
+@connect(createStructuredSelector({ user: makeSelectUser() }), (dispatch) => ({ dispatch }))
 export default class ProblemPage extends React.Component {
   static contextTypes = {
     styleManager: customPropTypes.muiRequired,
@@ -69,12 +92,14 @@ export default class ProblemPage extends React.Component {
   }
 
   parseProps(props) {
-    this.registerTabs();
+    this.registerTabs(props);
     this.ids = getIds(props);
   }
 
-  registerTabs() {
+  registerTabs(props) {
+    const { loading, contest } = props.data;
     this.tabs = [];
+    // if (loading) return;
 
     this.registerTab(
       CONTENT_HASH_PREFIX,
@@ -96,6 +121,14 @@ export default class ProblemPage extends React.Component {
       () => <FormattedMessage {...messages.questions} />,
       QuestionsPage
     );
+
+    if (!loading && contest.owners.map((o) => o.id).includes(props.user.id)) {
+      this.registerTab(
+        RESULTS_HASH_PREFIX,
+        () => <FormattedMessage {...messages.results} />,
+        ProblemResultsPage
+      );
+    }
   }
 
   registerTab(hash, header, body) {
@@ -146,6 +179,7 @@ export default class ProblemPage extends React.Component {
 }
 
 ProblemPage.propTypes = {
+  data: PropTypes.object.isRequired, // eslint-disable-line react/no-unused-prop-types
   location: PropTypes.object.isRequired,
   children: PropTypes.node,
   dispatch: PropTypes.func,

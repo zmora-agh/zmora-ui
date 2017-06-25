b811f02fbcf2bd4670c7153ec2b2d48909541b27
Marcin Moskal <mmos@memleak.pl>
2017-06-25 21:50:13 +0200

    Add contest ranking

---
+++ app/components/ContestRanking/messages.js
@@ -0,0 +1,16 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  rank: {
+    id: 'app.components.ContestRanking.rank',
+    defaultMessage: 'Rank',
+  },
+  name: {
+    id: 'app.components.ContestRanking.name',
+    defaultMessage: 'Name',
+  },
+  attempts: {
+    id: 'app.components.ContestRanking.attempts',
+    defaultMessage: 'Solved problems (attempts)',
+  },
+});

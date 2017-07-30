6b33fab3e24cc478b9ae91bf18760f4b5459356f
Mateusz Nowotyński <maxmati@memleak.pl>
2017-07-30 23:11:13 +0200

    Refactor HomePage

--- app/components/ZmoraCard/index.js
+++ app/components/ZmoraCard/index.js
@@ -14,7 +14,7 @@ function ZmoraCard(props) {
       style={{
         boxShadow: '6px 10px 10px rgba(0,0,0,0.19), 3px 6px 6px rgba(0,0,0,0.23)',
         backgroundColor: props.color,
-        height: props.height,
+        height: '100%',
       }}
     >
       <CardContent style={{ backgroundColor: props.color, padding: props.padding }}>
@@ -27,7 +27,6 @@ function ZmoraCard(props) {
 ZmoraCard.propTypes = {
   children: React.PropTypes.any,
   color: React.PropTypes.string,
-  height: React.PropTypes.number,
   padding: React.PropTypes.number,
 };
 

5ce1715dd09f4a8f754f1eed459c509c00331d31
Mateusz Nowotyński <maxmati@memleak.pl>
2017-07-31 15:46:11 +0200

    Add Sentry integration

--- app/constants.js
+++ app/constants.js
@@ -1,2 +1,3 @@
 export const API_URL = 'https://api.zmora-agh.memleak.pl/';
 export const GRAPHQL_API_URL = `${API_URL}graphql`;
+export const SENTRY_DSN = 'https://1cb4841ed2ab42aa8b77dd78d653fe3d@sentry.io/197753';

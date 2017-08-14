b45fe887a1ab746d1abc83e50c7a4bf9d99d77e9
Mateusz Nowotyński <maxmati@memleak.pl>
2017-08-14 14:00:40 +0200

    Switch mathjax source to cdnjs

--- app/components/Markdown/index.js
+++ app/components/Markdown/index.js
@@ -45,7 +45,7 @@ function Markdown(props) {
     <div
       className="markdown-body"
     >
-      <MathJax.Context>
+      <MathJax.Context script="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.1/MathJax.js?config=TeX-MML-AM_CHTML">
         <ReactMarkdown source={text} renderers={{ code: Code, code_block: CodeBlock }} />
       </MathJax.Context>
     </div>

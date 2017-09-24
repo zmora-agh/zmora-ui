36331fb7158783a175c11edecdb1838c0e404e74
Mateusz Nowotyński <maxmati@memleak.pl>
2017-09-24 20:28:29 +0200

    Show only contests in progress in upload dialog

---
+++ app/utils/model.js
@@ -0,0 +1,8 @@
+import moment from 'moment';
+
+export const isContestInProgress = (contest, serverTime) => {
+  const startTime = moment(contest.start).add(contest.signupDuration, 'seconds');
+  const endTime = moment(startTime).add(contest.duration, 'seconds');
+
+  return serverTime.isBetween(startTime, endTime);
+};

7a0078df9ec7601cd820ca56eb5b73e8d36ec484
Marcin Moskal <mmos@memleak.pl>
2017-10-06 23:48:02 +0200

    Allow Polish diacritics in name (#230)

--- app/components/RegisterForm/constants.js
+++ app/components/RegisterForm/constants.js
@@ -5,4 +5,4 @@
 export const EMAIL_PATTERN = /^([\w.+-]+)@([\w-]+\.)*([\w-]+)$/i;
 export const USERNAME_PATTERN = /^[a-z0-9._-]{3,15}$/i;
 export const PASSWORD_PATTERN = /^.{8,}$/i;
-export const NAME_PATTERN = /^[a-zA-z ]+$/i;
+export const NAME_PATTERN = /^[a-ząćęłńóśźż ]+$/i;
